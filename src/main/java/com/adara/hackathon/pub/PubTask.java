2e0092e60e4972c0c961d6e93cd158a7179c0bd3
yzhao <yi.zhao@adara.com>
2018-07-15 12:41:51 -0700

    retry

--- src/main/java/com/adara/hackathon/pub/PubTask.java
+++ src/main/java/com/adara/hackathon/pub/PubTask.java
@@ -3,11 +3,17 @@ package com.adara.hackathon.pub;
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
+import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.api.gax.rpc.ApiException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.ServiceOptions;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
@@ -24,33 +30,53 @@ public class PubTask implements Runnable {
     public void run() {
         // construct a pubsub message from the payload
 
-        for(int i = 0; i < 10; i++) {
-            String data = getUnixTimeStamp() + "|" + getUnixTimeStamp() + "|" + "hostname:" + getHostName() + ", current time:" + getCurrentDateTime();
-            PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(ByteString.copyFromUtf8(data)).build();
+        try {
+            for (int i = 0; i < 10; i++) {
+                String data = getUnixTimeStamp() + "|" + getUnixTimeStamp() + "|" + "hostname:" + getHostName() + ", current time:" + getCurrentDateTime();
+                PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(ByteString.copyFromUtf8(data)).build();
 
-            ApiFuture<String> future = publisher.publish(pubsubMessage);
-            // Add an asynchronous callback to handle success / failure
-            ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
+                ApiFuture<String> future = publisher.publish(pubsubMessage);
+                // Add an asynchronous callback to handle success / failure
+                ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
 
-                @Override
-                public void onFailure(Throwable throwable) {
-                    if (throwable instanceof ApiException) {
-                        ApiException apiException = ((ApiException) throwable);
-                        // details on the API exception
-                        System.out.println("apiException.getStatusCode().getCode():" + apiException.getStatusCode().getCode());
-                        System.out.println("apiException.isRetryable():" + apiException.isRetryable());
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        if (throwable instanceof ApiException) {
+                            ApiException apiException = ((ApiException) throwable);
+                            // details on the API exception
+                            System.out.println("apiException.getStatusCode().getCode():" + apiException.getStatusCode().getCode());
+                            System.out.println("apiException.isRetryable():" + apiException.isRetryable());
+                        }
+                        System.out.println("Error publishing message : " + data);
+                        log.info("[PubTask.run]" + "Error publishing message : " + data);
                     }
-                    System.out.println("Error publishing message : " + data);
-                    log.info("[PubTask.run]" + "Error publishing message : " + data);
-                }
 
-                @Override
-                public void onSuccess(String messageId) {
-                    // Once published, returns server-assigned message ids (unique within the topic)
-                    System.out.println("onSuccess with messageId:" + messageId + " , data:" + data);
-                    log.info("[PubTask.run]" + "onSuccess with messageId:" + messageId + " , data:" + data);
-                }
-            });
+                    @Override
+                    public void onSuccess(String messageId) {
+                        // Once published, returns server-assigned message ids (unique within the topic)
+                        System.out.println("onSuccess with messageId:" + messageId + " , data:" + data);
+                        log.info("[PubTask.run]" + "onSuccess with messageId:" + messageId + " , data:" + data);
+                    }
+                });
+            }
+        }catch(Exception e){
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource("adara-spore-drive-7a12bb7e0cfd.json").getFile());
+            String topicId = "fill-message-details";
+            // do nothing
+            ProjectTopicName topicName = ProjectTopicName.newBuilder()
+                    .setProject(ServiceOptions.getDefaultProjectId())
+                    .setTopic(topicId)
+                    .build();
+            GoogleCredentials credentials = null;
+            try {
+                credentials = GoogleCredentials.fromStream(
+                        new FileInputStream(file));
+                publisher = Publisher.newBuilder(topicName).setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+            }catch (Exception e2){
+
+            }
+
         }
     }
 
